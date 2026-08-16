ce5cdaf09f8e1a504223c894b31136a27a58ca69
Klaus Jørgensen <knj@sonarapp.com>
2026-08-16 14:35:08 +0200

    Gøglertilmelding lukket

--- go/cmd/api/home.go
+++ go/cmd/api/home.go
@@ -29,7 +29,7 @@ func (app *application) homeHandler(w http.ResponseWriter, r *http.Request) {
 		}{
 			{Label: "Tilmeld spejderpatrulje", URL: "/indskrivning/patrulje", Status: "OPEN"},
 			{Label: "Tilmeld seniorklan", URL: "/indskrivning/klan", Status: klanStatus},
-			{Label: "Tilmeld gøgler", URL: "/indskrivning/badut", Status: "OPEN"},
+			{Label: "Tilmeld gøgler", URL: "/indskrivning/badut", Status: "CLOSED"},
 		},
 	}
 	err := app.WriteJSON(w, http.StatusOK, jsonapi.Envelope{"config": config}, nil)
